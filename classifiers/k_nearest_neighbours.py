450c075e3fd07a065a575c4f0723905f4c2fd043
Gianluca Zani <zani.gianluca98@gmail.com>
2022-04-14 15:37:26 +1000

    Reverted back to working knn algorithm

--- classifiers/k_nearest_neighbours.py
+++ classifiers/k_nearest_neighbours.py
@@ -1,28 +1,25 @@
 import numpy as np
 import pandas as pd
 from assignment2.classes.ReverseFixedSizePriorityQueue import ReverseFixedSizePriorityQueue
-from assignment2.lib.knn_lib import euclidean_distance, get_best_k_neighbours_classes, predict_class, series_to_array
+from assignment2.lib.knn_lib import euclidean_distance, get_best_k_neighbours_classes, predict_class
 
 
 def classify_nn(training_filename, testing_filename, k):
+
     training_set = pd.read_csv(training_filename, header=None)  # training set as pandas.DataFrame
-    classes_column = training_set.iloc[:, -1]  # save classes before they get overwritten by nan in the np.genfromtxt(), will retrieve from here the class of training examlpes
+    classes_column = training_set.iloc[:, -1]
     training_set = np.genfromtxt(training_filename, delimiter=',', skip_header=0)
     testing_set = np.genfromtxt(testing_filename, delimiter=',', skip_header=0)
 
-    best_k_neighbours = ReverseFixedSizePriorityQueue(int(k))  # will keep only best k neighbours
-    result = list()  # predictions array to give as output
+    best_k_neighbours = ReverseFixedSizePriorityQueue(int(k))
+    result = list()
     for testing_example in testing_set:
         for index, training_example in enumerate(training_set):
-            distance = euclidean_distance(training_example[:-1],
-                                          testing_example)  # calculate distance between data points, keep last item of training example out (nan)
-            training_example_class = classes_column[
-                index]  # class of this training example taken from the classes column at the correspondent index
-            best_k_neighbours.put((distance, training_example_class))  # save (distance, class) tuple in priority queue
-        best_k_neighbours_classes = get_best_k_neighbours_classes(
-            best_k_neighbours)  # get best neighbour classes as array
-        predicted_class = predict_class(best_k_neighbours_classes)  # get majority class of best k neighbours
-        result.append(predicted_class)  # append oredicted class in result list
-        best_k_neighbours = ReverseFixedSizePriorityQueue(
-            int(k))  # instantiate new priority queue for the new example to examinate
+            distance = euclidean_distance(training_example[:-1], testing_example)
+            training_example_class = classes_column[index]
+            best_k_neighbours.put((distance, training_example_class))
+        best_k_neighbours_classes = get_best_k_neighbours_classes(best_k_neighbours)
+        predicted_class = predict_class(best_k_neighbours_classes)
+        result.append(predicted_class)
+        best_k_neighbours = ReverseFixedSizePriorityQueue(int(k))
     return result
--- experiments.py
+++ experiments.py
@@ -2,12 +2,13 @@ import queue
 
 import pandas as pd
 
+from assignment2.classifiers.k_nearest_neighbours import classify_nn
 from assignment2.stratified_folds_generation.stratified_cross_selection import generate_stratified_folds
 from classes.ReverseFixedSizePriorityQueue import ReverseFixedSizePriorityQueue
 from assignment2.classifiers import k_nearest_neighbours, naive_bayes
 # import time
 
-
+print(classify_nn("data/pima-indians-diabetes.csv", "data/test_set_knn.csv", 10))
 # print(naive_bayes.classify_nb("data/pima-indians-diabetes.csv", "data/test_set_knn.csv"))
-data_set = pd.read_csv("./data/test_stratified_folds.csv", header=None)
-generate_stratified_folds(data_set, 2)
+# data_set = pd.read_csv("./data/test_stratified_folds.csv", header=None)
+# generate_stratified_folds(data_set, 2)
